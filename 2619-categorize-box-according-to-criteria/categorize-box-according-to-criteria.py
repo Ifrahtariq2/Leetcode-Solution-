58b37b1ff1b13089296b0fee9922cfb941214205
Ifrah Tariq <tariqifrah2@gmail.com>
2024-08-16 19:34:21 +0500

    Time: 26 ms (92.35%) | Memory: 16.5 MB (72.68%) - LeetSync

---
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.py
@@ -0,0 +1,13 @@
+class Solution:
+    def categorizeBox(self, length: int, width: int, height: int, mass: int) -> str:
+        is_bulky = length >= 10000 or width >= 10000 or height >= 10000 or length * width * height >= 1000000000
+        is_heavy = mass >= 100
+
+        if is_bulky and is_heavy:
+            return "Both"
+        elif is_bulky:
+            return "Bulky"
+        elif is_heavy:
+            return "Heavy"
+        else:
+            return "Neither"
