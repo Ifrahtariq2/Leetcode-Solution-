474df4957697d16cb268e93bb0280f92cd1ea51b
Ifrah Tariq <tariqifrah2@gmail.com>
2025-07-24 11:56:53 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.30%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.py
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -1,21 +1,23 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        if s.count("A") < 2 :
-            if s.count("L") < 3:
-                return True
-            else:
+        if s.count("A") >= 2 :
+            return False
+            # if s.count("L") < 3:
+                # return True
+            # else:
                 
-                count = 0
-                for i in range(len(s)):
+        count = 0
+        for i in range(len(s)):
                     
-                    if s[i] == "L" :
-                        count += 1
-                    if count == 3:
-                        return False
-                    if s[i] != "L":
-                        count = 0
-                return True
-        return False
+            if s[i] == "L" :
+                count += 1
+                if count == 3:
+                    return False
+            else:
+    
+                count = 0
+        return True
+        
 
 
         
