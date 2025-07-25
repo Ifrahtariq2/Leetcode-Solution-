f16c71ddc9e9678cba90e8a378cdce9041f0e2ef
Ifrah Tariq <tariqifrah2@gmail.com>
2025-07-25 11:58:43 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.10%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.py
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.py
@@ -1,35 +1,27 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        grid = []
+        grid = [] # [["","",""],["","",""],["","",""]]
         for i in range(3):
-            grid.append([""]*3)
-        
+            grid.append([""] * 3)
         for i, (row, col) in enumerate(moves):
-            if i % 2 == 0:
-                player = 'A'
+            player = ""
+            if i % 2 == 0 :
+                player = "A"
             else:
-                player = 'B'
-            if player == 'A':
-                mark = 'X'
-            else:
-                mark = 'O'
-            grid[row][col] = mark
-            
-            # Check if the current player won
-            # Check rows
-            if grid[row][0] == grid[row][1] == grid[row][2] == mark:
+                player = "B"
+            grid[row][col] = player
+            if grid[row][0] == grid[row][1] == grid[row][2] == player:
                 return player
-            # Check columns
-            if grid[0][col] == grid[1][col] == grid[2][col] == mark:
+            elif grid[0][col] == grid[1][col] == grid[2][col] == player:
                 return player
-            # Check diagonals
-            if (row == col) and (grid[0][0] == grid[1][1] == grid[2][2] == mark):
+            elif (row == col) and grid[0][0] == grid[1][1] == grid[2][2] == player:
                 return player
-                #check anti-diagnol
-            if (row + col == 2) and (grid[0][2] == grid[1][1] == grid[2][0] == mark):
+            elif (row + col == 2) and grid[0][2] == grid[1][1] == grid[2][0] == player:
                 return player
         if len(moves) == 9:
-            return "Draw" 
+            return "Draw"
         else:
             return "Pending"
+        
+
         
